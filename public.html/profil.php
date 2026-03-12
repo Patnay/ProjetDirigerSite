5324044a7befaa2cabfc7db180f59e7e858f8ab6
Edflick68 <edstm88@gmail.com>
2026-03-12 12:01:55 -0400

    Create profil.php

---
+++ public.html/profil.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+   <meta charset="UTF-8">
+   <link rel="stylesheet" href="css/styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+</head>
+<body>
+  <div class="shop-page">
+    <header>
+      <?php include "header.php" ?>
+    </header>
+    <main class="shop-container">
+      <h1>Profil</h1>
+
+    </main>
+  </div>
+</body>
+</html>
