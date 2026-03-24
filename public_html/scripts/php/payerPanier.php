415aaaa93ce5c096f60ac482461f78fe2bcc2b01
Carl Patrice Antonio Paul <Jrcppaul@gmail.com>
2026-03-23 22:09:41 -0400

    addedcoms

--- public_html/scripts/php/payerPanier.php
+++ public_html/scripts/php/payerPanier.php
@@ -5,11 +5,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $idJoueur = $_SESSION['idJoueur'];
     $stmt = $pdo->prepare("CALL payerPanier(?)");
     $stmt->execute([$idJoueur]);
-    //header("Location: panier.php");
+    //header("Location:inventaire.php");
     echo '<script>location.reload();</script>';
     }
     catch(PDOException){
+        /*Doit implanter les erreur de manque de fonds
         
+UPDATE Joueurs
+SET nbOr=100,
+	nbArgent=100,
+    nbBronze=1000
+WHERE alias = 'Pat';
+
+        */
     }
     finally{
         exit;
