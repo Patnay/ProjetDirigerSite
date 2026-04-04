4ecd5e0eafb5595a1d1eb55eb61ef3ea7a02cab0
EldenRing1234 <emmanueldouville24@gmail.com>
2026-04-04 18:06:54 -0400

    Plus de criss d'italique dans le header de boutique

--- public_html/footer.php
+++ public_html/footer.php
@@ -1,5 +1,5 @@
 <style>
-    a{ 
+    footer a{ 
         display: block;        
         text-align: center;
         color: #C6A667;
@@ -7,4 +7,6 @@
         font-style: italic;
     }
 </style>
+<footer>
 <a>2026 Projet Dirigé - B.F.G.D - Carl Patrice Antonio Paul, Emmanuel Douville, Edouard St-Martin, William Lavoie</a>
+</footer>
