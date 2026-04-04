e331b955aa2244ed5fa4ea53647a2a1d5a956bb5
EldenRing1234 <emmanueldouville24@gmail.com>
2026-04-04 19:06:47 -0400

    Fix Inscriptions

--- public_html/inscription.php
+++ public_html/inscription.php
@@ -1,12 +1,6 @@
 <?php
-try {
-    ini_set('display_errors', 'Off');
-    ini_set('log_errors', 'On');
-    error_reporting(E_ALL);
-    session_start();
-} catch (Exception) {
-
-}
+  session_start();
+  include("scripts/php/formulaire/scriptInscription.php");
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -14,18 +8,18 @@ try {
 
 <head>
   <meta charset="UTF-8">
+  <link rel="icon" type="image/png" href="favicon.png">
   <link rel="stylesheet" href="css/styles.css">
   <link rel="stylesheet" href="css/connexion.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Inscription</title>
 </head>
 
-<body class="shop-page">
-  <div>
-    <header>
-      <?php include "header.php" ?>
-    </header>
-    <main class="connect-container">
+<?php include "header.php" ?>
+
+<body>
+<main class="shop-page">
+  <div class="connect-container">
       <h1>Se créer un compte</h1>
       <form action="inscription.php" method="POST">
         <fieldset class="connect-block">
@@ -57,12 +51,55 @@ try {
         </fieldset>
         <br>
       </form>
-
-      <?php include("scripts/php/formulaire/scriptInscription.php")?>
-
+    <?php
+    if (!empty($_SESSION['erreurs_inscription'])) {
+        echo '<div class="erreurs-inscription">';
+        echo '<h2>Erreurs :</h2><ul>';
+        foreach ($_SESSION['erreurs_inscription'] as $e) {
+            echo "<li>$e</li>";
+        }
+        echo '</ul></div>';
+        unset($_SESSION['erreurs_inscription']);
+    }
+    ?>
+  <br>
       <button class="connect-button" onclick="window.location.href = 'connexion.php'">Déjà connecté? Connectez-vous</button>
-    </main>
   </div>
+  
+<!-- Bouton musique -->
+<img id="musicToggle" 
+     src="image/sonOff.jpg" 
+     style="
+        position: fixed;
+        bottom: 20px;
+        right: 20px;
+        width: 60px;
+        height: 60px;
+        cursor: pointer;
+        z-index: 9999;
+     ">
+<audio id="bgMusic" loop>
+    <source src="musique/eldenRing.mp3" type="audio/mp3">
+</audio>
+<script>
+const music = document.getElementById("bgMusic");
+const toggleBtn = document.getElementById("musicToggle");
+
+let musicOn = false;
+
+toggleBtn.addEventListener("click", () => {
+    musicOn = !musicOn;
+
+    if (musicOn) {
+        music.play();
+        toggleBtn.src = "image/sonOn.jpg";
+    } else {
+        music.pause();
+        toggleBtn.src = "image/sonOff.jpg";
+    }
+});
+</script>
+</main>
 </body>
 
 </html>
